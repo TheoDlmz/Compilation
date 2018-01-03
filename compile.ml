a071e022a98fe8bf6c6b9af41dc5ad80aa1ab1a3
TheoDlmz <34279251+TheoDlmz@users.noreply.github.com>
2018-01-03 21:25:36 +0100

    Update compile.ml

--- compile.ml
+++ compile.ml
@@ -16,13 +16,15 @@ type structs = (tipe Smap.t) Smap.t
 
 let structlist (i,t) S = Smap.add i t S
 
+let segment_donnes = ref nop
+let string_count = ref 0
 
 let rec alloc_expr env next = function
 	|Cint i -> Cst i, next
 	|Cbool b -> Cbool b, next
 	|Cident x -> begin try 
 			let ofs_x = Smap.find x env in
-				Cident (ofs_x,,next
+				Cident ofs_x,next
 		      with Not_found -> raise (VarUndef x)
 	|Binop(o,e1,e2) -> let e1,fpmax1 = alloc_expr env next e1 in
 				let e2,fpmax2 = alloc_expr env next e2 in
@@ -43,7 +45,7 @@ let rec compile_expr expr = label_count := !label_count + 1;
 		match expr with
 	|Cint i -> pushq (imm i)
 	|Cbool b -> if b then pushq (imm 1) else pushq (imm 0)
-	|Cident x ->
+	|Cident fp_x -> pushq (ind ~ofs:fp_x rbp)
 	|Binop (o,e1,e2) ->
 		let code1 = compile_expr e1 in
 		let code2 = compile_expr e2 in
@@ -92,7 +94,7 @@ let rec compile_expr expr = label_count := !label_count + 1;
 			|Mod -> cqto ++ idivq (reg rbx)
 				++ movq (reg rdx) (reg rax))
 		++ pushq (reg rax)
-		|Egal -> 
+		|Egal -> (* WTFUCKKKKKKKKKKKKKKKKKKKKKK *)
 	|Unop (u,e) -> compile_expr e ++ popq rax ++
 			(match u with
 			   |Neg -> negq (reg rax)
@@ -101,7 +103,7 @@ let rec compile_expr expr = label_count := !label_count + 1;
 			   |Ref -> 
 			   |RefMut ->
 			   ) ++ pushq (reg rax)
-	|Cselect (e,x) -> (* e.x *)
+	|Cselect (e,x) -> (* e.x  chiaaaaanttt *)
 	|Clen e -> compile_expr e++
 		   popq (reg rbx) ++
 		   movl (ind ~ofs:8 rbx) (reg rax)
@@ -111,14 +113,20 @@ let rec compile_expr expr = label_count := !label_count + 1;
 			 movl (ind ~ofs:16 rax) (reg rbx) ++
 			 (* doit recuperer la taille du type des elems de e1 puis multiplier par e2 et le renvoyer *)
 			 
-	|Ccall (x,l) -> (*appel de fonction*)
+	|Ccall (x,l) -> (*appel de fonction*) (* on mets tous les args dans la pile *) ++ call x ++ (* on desallou la pile *)
 	|Cvec l -> let n = List.length l (* construit le vecteur l, n est son premier element et n*t l'espace alloué sur le tas *)
-	|Fprint s -> (* rajouter s dans le segment des données puis appeler print dessus *)
+	|Fprint s -> begin
+			string_count := !string_count + 1;
+			segment_donnes := !segment_donnes ++ label ("chaine_"^string_of_int(string_count)) ++ string (s(*^"\\n"*));
+			mov (imm s) (reg rdi) ++
+			movq (imm 0) (reg rax) ++
+			call printf
+		      end
 	|Cbloc b -> compile_bloc b
 	|Cexpr e -> compile_expr e
 	
 and let rec compile_bloc = function
-	|EmptyBloc -> ()
+	|EmptyBloc -> nop
 	|E e -> compile_expr e
 	|I i -> compile_instr i
 	|B (i,b) -> compile_instr i ++ compile_bloc b
@@ -126,10 +134,11 @@ and let rec compile_bloc = function
 and let rec compile_instr instr = label_count := !label_count + 1; 
 	let label_string = string_of_int !label_count in
 	match instr with
-	|Inone -> ()
+	|Inone -> nop
 	|Iexpr e -> compile_expr e
-	|Iinit (x,e) ->
-	|IinitS (x,y,l) ->
+	|Iinit (ofs_x,e) -> compile_expr e ++ popq rax
+			++ movq (reg rax) (ind ~ofs:fp_x rbp)
+	|IinitS (x,y,l) -> (* INIT STRUCT CHIANT *)
 	|Iwhile (e,b) ->
 		label ("w_deb_"^label_string) ++
 		compile_expr e ++
@@ -139,7 +148,7 @@ and let rec compile_instr instr = label_count := !label_count + 1;
 		compile_bloc b ++
 		jmp ("w_deb_"^label_string) ++
 		label ("w_end_"^label_string)
-	|Ireturn e ->
+	|Ireturn e -> compile_expr e ++ popq rax ++ ret
 	|Iif i -> compile_if i
 	
 and let rec compile_if i = label_count := !label_count + 1; 
@@ -167,16 +176,28 @@ and let rec compile_if i = label_count := !label_count + 1;
 			compile_if i2 ++
 			label ("if_end_"^label_string))
 
-let compile_fun df =
+
+	
 
 
 
 let compile_decl d = match d with
-	|Dfun df -> compile_fun df
-	|Dstruct ds -> ()
+	|Dfun df -> match df.nom with
+		"Main" -> (label "main" ++ compile_bloc df.bloc,nop)
+		|_ -> (* on recupere les arguments sur la pile blabla *)
+			(nop,label df.nom ++ compile_bloc df.bloc)
+			(* on désalloue la pile ou jspas quoi *)
+	|Dstruct ds -> (nop,nop) (**)
 
 
 
 let compile_program p ofile =
-	let p = alloc p in
-	let code = List.fold_left compile_stmt 
+	let f = alloc f in
+	let (codemain, codefuns) = List.fold_left compile_decl  (nop, nop) f in
+		let p =
+		{ text = globl "main" ++ codemain ++
+		movq (imm 0) (reg rax) ++
+		ret ++
+		codefuns;
+		data = 	!segment_donnes
+		
